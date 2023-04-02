47dfa97fc3854a408923b4f3a3c734d622e9b5cb
RaphaelVR <raphaelventura55@gmail.com>
2023-04-02 16:55:46 +0200

    fixed small bugs & sidebar

--- twitter-clone/components/layout/Sidebar.tsx
+++ twitter-clone/components/layout/Sidebar.tsx
@@ -1,26 +1,45 @@
-import { BsMouseFill } from 'react-icons/bs';
+import { BsHouseFill, BsBellFill } from 'react-icons/bs';
 import { FaUser } from 'react-icons/fa';
+import { BiLogOut } from "react-icons/bi";
 
 import SidebarLogo from './SidebarLogo';
+import SidebarItem from './SidebarItem';
+import SidebarTweetButton from "./SidebarTweetButton";
 
 const Sidebar = () => {
-    const itens = [
+    const items = [
     {
         label: 'Home',
         href: '/',
-        icon: BsMouseFill,
+        icon: BsHouseFill,
     },
     {
         label: 'Notifications',
         href: '/notifications',
         icon: FaUser,
+    },
+    {
+        label: 'Profile',
+        href: '/user/123',
+        icon: BsBellFill,
     }
+
 ]
   return (
     <div className='col-span-1 h-full pr-4 md:pr-6'>
         <div className='flex flex-col items-end'>
             <div className='space-y-2 lg:w-[230px]'>
                 <SidebarLogo />
+                {items.map((item) =>  (
+                    <SidebarItem 
+                        key={item.href}
+                        href={item.href}
+                        label={item.label}
+                        icon={item.icon} 
+                    />
+                ))}
+                {/* <Sidebar onClick={() => {}} icon={BiLogOut} label='Logout'  /> */}
+                <SidebarTweetButton />
             </div>
         </div>
     </div>
--- twitter-clone/components/layout/SidebarLogo.tsx
+++ twitter-clone/components/layout/SidebarLogo.tsx
@@ -4,8 +4,8 @@ import { BsTwitter } from 'react-icons/bs';
 const SidebarLogo = () => {
     const router = useRouter(); 
   return (
-    <div>
-
+    <div className='rounded-full h-14 2-14 p-4 flex items-center hover:bg-blue-300 hover:bg-opacity-10 cursor-pointer transition'>
+        <BsTwitter size={28} color='white'/>
     </div>
   )
 }
