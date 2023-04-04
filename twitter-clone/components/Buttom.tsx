394bd7a9e00c884fa1cabf2acb2af866f60e88a3
RaphaelVR <raphaelventura55@gmail.com>
2023-04-04 15:38:34 +0200

    components

---
+++ twitter-clone/components/Buttom.tsx
@@ -0,0 +1,17 @@
+interface ButtomProps {
+    label: string;
+    secondary: boolean;
+    fullWidth: boolean;
+    large: boolean;
+    onClick: () => void;
+    disabled: boolean;
+    outline: boolean;
+}
+
+const Buttom: React.FC<ButtomProps> = ({ label, secondary, fullWidth, large, onClick, disabled, outline}) => {
+  return (
+    
+  )
+}
+
+export default Buttom
