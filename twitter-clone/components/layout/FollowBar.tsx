1f58b2f269faf993ffbd52f8061b0a01d4375f02
RaphaelVR <raphaelventura55@gmail.com>
2023-06-14 10:22:14 +0200

    user fetcher & follow bar

--- twitter-clone/components/layout/FollowBar.tsx
+++ twitter-clone/components/layout/FollowBar.tsx
@@ -1,6 +1,14 @@
+import useUsers from "@/hooks/useUsers"
+import Avatar from "../Avatar";
 
 
 const FollowBar = () => {
+  const { data: users = [] } = useUsers();
+
+  if (users.length === 0) {
+    return null;
+  }
+
   return (
     <div className="px-6 py-4 hidden lg:block">
         <div className="bg-neutral-800 rounded-xl p-4">
@@ -9,6 +17,16 @@ const FollowBar = () => {
             </h2>
             <div className="flex flex-col gap-6 mt-4">
               {/* TODO USERS */}
+              {users.map((user: Record<string, any>) => (
+                <div key={users.id} className="flex flex-row gap-4">
+                  <Avatar userId={""}/>
+                  <div className="flex flex-col">
+                    <p className="text-white font-semibold text-sm">
+                      {user.name}
+                    </p>
+                  </div>
+                </div>
+              ))}
             </div>
         </div>
     </div>
--- twitter-clone/hooks/useCurrentUser.ts
+++ twitter-clone/hooks/useCurrentUser.ts
@@ -11,6 +11,7 @@ const useCurrentUser = () => {
     isLoading,
     mutate
   }
+  
 };
 
 export default useCurrentUser;
