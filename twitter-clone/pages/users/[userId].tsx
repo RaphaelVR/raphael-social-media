2234e6d230b03b59364142f4c888e6879eeac86a
RaphaelVR <raphaelventura55@gmail.com>
2023-06-27 09:15:18 +0200

    User Hero

--- twitter-clone/pages/users/[userId].tsx
+++ twitter-clone/pages/users/[userId].tsx
@@ -1,19 +1,33 @@
-import Header from "@/components/Header";
-import useUser from "@/hooks/useUser";
 import { useRouter } from "next/router";
+import { ClipLoader } from "react-spinners";
+
+import useUser from "@/hooks/useUser";
+
+import Header from "@/components/Header";
+import UserHero from "@/components/users/UserHero";
+
+
 
 const UserView = () => {
-    const router = useRouter();
-    const { userId } = router.query;
+  const router = useRouter();
+  const { userId } = router.query;
 
-    const { data: fetchedUser, isLoading } = useUser(userId as string);
-    
-     
+  const { data: fetchedUser, isLoading } = useUser(userId as string);
+
+  if (isLoading || !fetchedUser) {
+    return (
+      <div className="flex justify-center items-center h-full">
+        <ClipLoader color="lightblue" size={80} />
+      </div>
+    )
+  }
+
   return (
     <>
-        <Header showBackArrow label="User Profile"/>
+      <Header showBackArrow label={fetchedUser?.name} />
+      <UserHero userId={userId as string} />
     </>
-  )
+   );
 }
-
+ 
 export default UserView;
