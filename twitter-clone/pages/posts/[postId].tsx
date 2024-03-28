61d1a628a97ccb8b82198f827c063f988f5c2279
RaphaelVR <raphaelventura55@gmail.com>
2024-03-28 16:21:18 +0100

    Post fixed

--- twitter-clone/pages/posts/[postId].tsx
+++ twitter-clone/pages/posts/[postId].tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useRouter } from "next/router";
+import { ClipLoader } from "react-spinners";
+
+import Form from "@/components/Form";
+import Header from "@/components/Header";
+import PostItem from "@/components/posts/PostItem";
+import usePost from "@/hooks/usePost";
 
 const PostView = () => {
-  return (
-    <div>PostView</div>
-  )
+    const router = useRouter();
+    const { postId } = router.query;
+
+    const { data: fetchedPost, isLoading } = usePost(postId as string);
+
+    if (isLoading || fetchedPost) {
+        return (
+            <div className="flex justify-center items-center h-full">
+                <ClipLoader />
+            </div>
+        )
+    }
+
+return (
+    <>
+            <Header label='Tweet' showBackArrow />
+            <PostItem data={fetchedPost} userId={fetchedPost.user.id} />
+            <Form
+                    postId = {postId as string}
+                    isComment
+                    placeholder="Tweet your reply"
+            />
+    </>
+);
 }
 
-export default PostView
+export default PostView;
