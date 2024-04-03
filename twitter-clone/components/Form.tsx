ef618a8a5563e1f4ffeac4ae71914902f58e4596
RaphaelVR <raphaelventura55@gmail.com>
2024-04-03 13:01:13 +0200

    Notifications done

--- twitter-clone/components/Form.tsx
+++ twitter-clone/components/Form.tsx
@@ -107,7 +107,7 @@ const Form: React.FC<FormProps> = ({
         ) : (
             <div className="py-8">
                 <h1 className="text-white text-2xl text-center mb-4 font-bold">
-                    Welcome to Raphael`s Twitter  =D
+                    Welcome to Raphael`s Social Media  =D
                 </h1>
                 <div className="flex flex-row items-center justify-center gap-4">
                     <Button
